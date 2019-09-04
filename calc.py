4eea9797e4518b0c00d9dd2c12574a4f14a7b661
Kagome Miller <kagomemiller@gmail.com>
2019-09-03 21:56:36 -0700

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,36 @@
+def add(x, y):
+  return x + y
+def sub(x, y):
+    return x - y
+def mult(x, y):
+    return x * y
+def divide(x, y):
+    return x / y
+
+print('Select Function...')
+print('1. Add')
+print('2. Subtract')
+print('3. Multiply')
+print('4. Divide')
+
+# number choice depends on user input
+
+choice = input('Enter Choice(1/2/3/4: ')
+
+num1 = float(input('Enter First Number: '))
+num2 = float(input('Enter Second Number: '))
+
+if choice == '1':
+    print(num1, '+', num2,'=', add(num1,num2))
+
+elif choice == '2':
+    print(num1, '-', num2, '=', subtract(num1,num2))
+
+elif choice == '3':
+    print(num1, '*', num2, '=', multiply(num1,num2))
+
+elif choice == '4':
+    print(num1, '/', num2, '=', divide(num1,num2))
+
+else:
+    print('Invalid Input')
